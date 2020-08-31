779ef33310cfb6b3a86600da775998fd16f14be5
niconap <nico.nap@outlook.com>
2020-08-31 14:37:53 +0200

    Add delete button and checkbox

--- src/index.js
+++ src/index.js
@@ -8,6 +8,23 @@ let tasks = [];
 // Create a default project with a default task
 projects.push("default");
 tasks.push(makeTask("default", "default task", "none", "01-01-2020", "high"));
+tasks.push(makeTask("default", "default task", "none", "01-01-2020", "low"));
 
 // Render the projects with their tasks
 render(projects, tasks);
+createButtons();
+
+// Insert "Delete" buttons into each task and give them event listeners
+function createButtons() {
+    for (let i = 0; i < tasks.length; i++) {
+        let currentTask = document.getElementById(i);
+        let deleteButton = document.createElement("button");
+        deleteButton.addEventListener("click", function(){
+            tasks.splice(i, 1);
+            render(projects, tasks);
+            createButtons();
+        });
+        deleteButton.innerHTML = "Delete";
+        currentTask.appendChild(deleteButton);
+    }
+}
--- src/modules/render.js
+++ src/modules/render.js
@@ -1,42 +1,66 @@
 const render = function(projects, tasks) {
+    let index = 0;
     let container = document.getElementById("content");
-    projects.forEach(project => {
-        let projectContainer = document.createElement("div");
-        projectContainer.setAttribute("id", project);
-        container.appendChild(projectContainer);
-        projectContainer = document.getElementById(project);
-        let projectTitle = document.createElement("h1");
-        projectTitle.innerHTML = project;
-        projectContainer.appendChild(projectTitle);
-    })
-    tasks.forEach(task => {
-        let projectName = task.project;
-        let projectContainer = document.getElementById(projectName);
-        let taskTitle = task.title;
-        let taskDescription = task.description;
-        let taskDue = task.due;
-        let taskPriority = task.priority;
+    container.innerHTML = "";
+    
+    // Render the project containers
+    if(projects.length != 0) {
+        projects.forEach(project => {
+            let projectContainer = document.createElement("div");
+            projectContainer.setAttribute("id", project);
+            container.appendChild(projectContainer);
+            projectContainer = document.getElementById(project);
+            let projectTitle = document.createElement("h1");
+            projectTitle.innerHTML = project;
+            projectContainer.appendChild(projectTitle);
+        })
+    }
 
-        let title = document.createElement("p");
-        title.setAttribute("class","tasktitle");
-        title.innerHTML = taskTitle;
-        projectContainer.appendChild(title);
+    // Render the task containers and put them in the right project container
+    if(tasks.length != 0) {
+        tasks.forEach(task => {
+            let projectName = task.project;
+            let projectContainer = document.getElementById(projectName);
+            let taskWrapper = document.createElement("div");
+            taskWrapper.setAttribute("id",index);
+            projectContainer.appendChild(taskWrapper);
+            let taskTitle = task.title;
+            let taskDescription = task.description;
+            let taskDue = task.due;
+            let taskPriority = task.priority;
 
-        let description = document.createElement("p");
-        description.setAttribute("class","taskelement");
-        description.innerHTML = taskDescription;
-        projectContainer.appendChild(description);
+            let title = document.createElement("p");
+            title.setAttribute("class","tasktitle");
+            title.innerHTML = taskTitle;
+            taskWrapper.appendChild(title);
 
-        let due = document.createElement("p");
-        due.setAttribute("class","taskelement");
-        due.innerHTML = taskDue;
-        projectContainer.appendChild(due);
-        
-        let priority = document.createElement("p");
-        priority.setAttribute("class","taskelement");
-        priority.innerHTML = taskPriority;
-        projectContainer.appendChild(priority);
-    });
+            let description = document.createElement("p");
+            description.setAttribute("class","taskelement");
+            description.innerHTML = taskDescription;
+            taskWrapper.appendChild(description);
+
+            let due = document.createElement("p");
+            due.setAttribute("class","taskelement");
+            due.innerHTML = taskDue;
+            taskWrapper.appendChild(due);
+            
+            let priority = document.createElement("p");
+            priority.setAttribute("class","taskelement");
+            priority.innerHTML = taskPriority;
+            taskWrapper.appendChild(priority);
+
+            let checkbox = document.createElement("input");
+            checkbox.setAttribute("type","checkbox");
+            checkbox.setAttribute("id","check" + index);
+            taskWrapper.appendChild(checkbox);
+
+            let label = document.createElement("label");
+            label.setAttribute("for","checkbox" + index);
+            label.innerHTML = "Done";
+            taskWrapper.appendChild(label);
+            index += 1;
+        });
+    }
 }
 
 export default render
